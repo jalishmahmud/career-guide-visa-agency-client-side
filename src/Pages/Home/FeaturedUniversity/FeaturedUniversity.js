e5b176f8603bddfc0f42361e25986d3fe907eb4f
jalishmahmud <jalish93@gmail.com>
2022-04-07 02:39:59 +0600

    featured university added on homepage

--- src/Pages/Home/FeaturedUniversity/FeaturedUniversity.js
+++ src/Pages/Home/FeaturedUniversity/FeaturedUniversity.js
@@ -1,5 +1,23 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import logo1 from "../../../images/university-logo/logo-1.png";
+import logo10 from "../../../images/university-logo/logo-10.png";
+import logo11 from "../../../images/university-logo/logo-11.png";
+import logo12 from "../../../images/university-logo/logo-12.png";
+import logo13 from "../../../images/university-logo/logo-13.png";
+import logo14 from "../../../images/university-logo/logo-14.png";
+import logo15 from "../../../images/university-logo/logo-15.png";
+import logo16 from "../../../images/university-logo/logo-16.png";
+import logo17 from "../../../images/university-logo/logo-17.png";
+import logo18 from "../../../images/university-logo/logo-18.png";
+import logo19 from "../../../images/university-logo/logo-19.png";
+import logo2 from "../../../images/university-logo/logo-2.png";
+import logo3 from "../../../images/university-logo/logo-3.png";
+import logo4 from "../../../images/university-logo/logo-4.png";
+import logo5 from "../../../images/university-logo/logo-5.png";
+import logo6 from "../../../images/university-logo/logo-6.png";
+import logo8 from "../../../images/university-logo/logo-8.png";
+import logo9 from "../../../images/university-logo/logo-9.png";
 import "./FeaturedUniversity.css";
 const FeaturedUniversity = () => {
   return (
@@ -10,7 +28,60 @@ const FeaturedUniversity = () => {
           <h5>We have selected best universities for your hight education</h5>
         </div>
         <Row>
-          <Col md={2}></Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo1} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo2} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo3} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo4} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo5} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo6} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo19} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo8} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo9} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo10} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo11} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo12} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo13} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo14} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo15} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo16} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo17} alt="" />
+          </Col>
+          <Col md={2}>
+            <img className="img-fluid" src={logo18} alt="" />
+          </Col>
         </Row>
       </Container>
     </div>
